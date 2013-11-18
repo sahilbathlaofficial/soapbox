ffb095473cbac9a91b5831f0be440ce63521b34c
Sahil Bathla <sahil@vinsol.com>
2013-11-18 19:58:34 +0530

    Notification concern moved from controller to model

--- app/controllers/site_admin/companies_controller.rb
+++ app/controllers/site_admin/companies_controller.rb
@@ -1,18 +1,19 @@
 # CR_Priyank: Indent properly
 class SiteAdmin::CompaniesController < SiteAdmin::AdminController
+  
   def show
     @companies = Company.all
   end
 
   def manage_companies
-    # CR_Priyank: I think we shall use require instead of permit here and handle exception accordingly
-   allowed_params = params.permit('to_ban')
-   allowed_params[:to_ban].split.each do |company_id|
+  # CR_Priyank: I think we shall use require instead of permit here and handle exception accordingly
+  allowed_params = params.permit('to_ban')
+  allowed_params[:to_ban].split.each do |company_id|
     # CR_Priyank: Use where instead of find
     # CR_Priyank: What if object is not destroyed ?
-     Company.find(company_id).destroy
+    Company.find(company_id).destroy
      # CR_Priyank: Fix typo
-     flash[:notice]= 'Comapnies Destroyed'
+      flash[:notice]= 'Companies Destroyed'
       respond_to do |format|
         format.html { redirect_to action: 'show'}
       end
