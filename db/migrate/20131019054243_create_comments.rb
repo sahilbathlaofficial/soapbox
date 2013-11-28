60b65d1e4907af69bb7982852359979baa957b1e
Sahil <sahil@vinsol.com>
2013-11-28 19:36:12 +0530

    Site admin can now remove moderators, tree heirarchy from company-groups-users for admin shown

--- db/migrate/20131019054243_create_comments.rb
+++ db/migrate/20131019054243_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.string :content
+      t.text :content
       t.references :user, index: true
       t.references :post, index: true
 
