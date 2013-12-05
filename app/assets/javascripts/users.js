d47a9c4c3d6b8838c24014fca30ac593a19d2303
Sahil <sahil@vinsol.com>
2013-12-05 19:29:39 +0530

    Edit profile layout fixed

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+  if($('#apiToken').text().trim() === '')
+    $('#apiToken').hide()
+})
--- app/views/users/api_token.js
+++ app/views/users/api_token.js
@@ -2,3 +2,4 @@ $apiTokenBox = $('#apiToken')
 $apiTokenBox.html('')
 $apiTokenBox.append('<p>Consumer Token: <%= current_user.consumer_key %></p>')
 $apiTokenBox.append('<p>Consumer Secret: <%= current_user.consumer_secret %></p>')
+$apiTokenBox.show();
