2f68ab8bba2d1e617938a78da86ceeb1c2e7205a
Sahil <sahil@vinsol.com>
2013-12-13 15:20:35 +0530

    Only approved group posts should be viewed feature added

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
   def extract_viewable_posts(user, current_user)
-    user.posts.where('group_id is ? or group_id in (?)', nil, current_user.groups)
+    user.posts.where('group_id is ? or group_id in (?)', nil, current_user.group_memberships.with_state(:approved).collect(&:group) )
   end
 end
